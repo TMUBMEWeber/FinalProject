d34058c74420cde3eee7f0d45792152f10af1956
陳椲博 <chenwebwe@chenweibodeMacBook-Air.local>
2022-06-07 12:59:09 +0800

    2

---
+++ final_project.h
@@ -0,0 +1,13 @@
+//
+//  final_project.h
+//  final_project
+//
+//  Created by 陳椲博 on 2022/6/7.
+//
+
+#ifndef final_project_h
+#define final_project_h
+
+#include <stdio.h>
+
+#endif /* final_project_h */
---
+++ final_project/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  final_project
+//
+//  Created by 陳椲博 on 2022/6/7.
+//
+
+import Foundation
+
+print("Hello, World!")
+
